2142bb8fb09115aa9f0f39884b7686b09a501651
Alaric Whitehead <alaric.whitehead@gmail.com>
2021-04-01 11:13:48 +0100

    feat: added a lexer for the language

---
+++ src/lexer.ts
@@ -0,0 +1,90 @@
+import LexerException from "./exceptions/LexerException";
+
+type SYMBOL_TOKEN = '<' | '>' | '[' | ']' | '(' | ')' | '|' | '.' | ',' | '...';
+
+//Symbols
+export const TKN_DOT: SYMBOL_TOKEN = '.';
+export const TKN_DOTS: SYMBOL_TOKEN = '...';
+export const TKN_BAR: SYMBOL_TOKEN = '|';
+export const TKN_COMMA: SYMBOL_TOKEN = ',';
+export const TKN_TREE_OPN: SYMBOL_TOKEN = '<';
+export const TKN_TREE_CLS: SYMBOL_TOKEN = '>';
+export const TKN_LIST_OPN: SYMBOL_TOKEN = '[';
+export const TKN_LIST_CLS: SYMBOL_TOKEN = ']';
+export const TKN_PREN_OPN: SYMBOL_TOKEN = '(';
+export const TKN_PREN_CLS: SYMBOL_TOKEN = ')';
+const SYMBOL_LIST = [
+	TKN_DOTS, TKN_DOT,
+	TKN_COMMA, TKN_BAR,
+	TKN_TREE_OPN, TKN_TREE_CLS,
+	TKN_LIST_OPN, TKN_LIST_CLS,
+	TKN_PREN_OPN, TKN_PREN_CLS,
+];
+
+//Atoms
+export type TKN_ATOM = string;
+//All tokens
+export type TOKEN = SYMBOL_TOKEN | TKN_ATOM;
+
+export default function lexer(converterString: string) : TOKEN[] {
+	let res: TOKEN[] = [];
+	let pos = 0;
+	while (converterString.length) {
+		//Remove any whitespace characters from the start of the string
+		let startingLength = converterString.length;
+		converterString = converterString.trimStart()
+		pos += startingLength - converterString.length;
+
+		//Stop if the remaining characters were all whitespace
+		if (!converterString) break;
+
+		//Check to see if the next token is a symbol
+		let token: SYMBOL_TOKEN | null = _checkForSymbolToken(converterString);
+		if (token !== null) {
+			res.push(token);
+			pos += token.length;
+			converterString = converterString.substr(token.length);
+		} else {
+			let atom: TKN_ATOM|null = _checkForAtomToken(converterString);
+			if (atom == null) {
+				throw new LexerException(`SyntaxError: Unexpected token '${converterString.charAt(0)}' at position ${pos}`);
+			} else {
+				res.push(atom);
+				pos += atom.length;
+				converterString = converterString.substr(atom.length);
+			}
+		}
+	}
+	return res;
+}
+
+/**
+ * Get the symbol token matching the start of the provided string.
+ * @param str	The string to check
+ * @example {@code '<nil>'} returns {@code '<'}
+ * @example {@code 'nil>'} returns {@code null}
+ * @example {@code '[]'} returns {@code '['}
+ * @returns	The matching token string, or {@code null} if there isn't a match
+ */
+function _checkForSymbolToken(str: string) : SYMBOL_TOKEN|null {
+	for (let token of SYMBOL_LIST) {
+		if (str.substr(0, token.length) === token) return token
+	}
+	return null;
+}
+
+/**
+ * Get an atom matching the start of the provided string.
+ * This will be the longest string consisting of the alphanumeric characters (a-z, 0-9) in any case, and '_'.
+ * @param str	The string to check
+ * @example {@code '<nil>'} returns {@code null}
+ * @example {@code 'nil>'} returns {@code nil}
+ * @example {@code 'nilish'} returns {@code 'nilish'}
+ * @example {@code 'my atom'} returns {@code 'my'}
+ * @returns	The matching atom string, or {@code null} if there isn't a match
+ */
+function _checkForAtomToken(str: string) : TKN_ATOM|null {
+	let match: RegExpExecArray | null = /^[a-z0-9_]+/i.exec(str);
+	if (!match) return null;
+	return match[0];
+}
---
+++ test/lexer.test.ts
@@ -0,0 +1,218 @@
+import { expect } from "chai";
+import { describe, it } from "mocha";
+import lexer, {
+	TKN_BAR,
+	TKN_COMMA,
+	TKN_DOT,
+	TKN_DOTS,
+	TKN_LIST_CLS,
+	TKN_LIST_OPN,
+	TKN_PREN_CLS,
+	TKN_PREN_OPN,
+	TKN_TREE_CLS,
+	TKN_TREE_OPN,
+	TOKEN
+} from "../src/lexer";
+
+describe('Lexer (whitespace)', function () {
+	describe(`''`, function () {
+		it('should produce an empty list', function () {
+			const actual: TOKEN[] = lexer('');
+			expect(actual).to.eql(
+				[]
+			);
+		});
+	});
+	describe(`'    '`, function () {
+		it('should produce an empty list', function () {
+			const actual: TOKEN[] = lexer('    ');
+			expect(actual).to.eql(
+				[]
+			);
+		});
+	});
+	describe(`'    nil    '`, function () {
+		it(`should produce the 'nil' token`, function () {
+			const actual: TOKEN[] = lexer('    nil    ');
+			expect(actual).to.eql(
+				['nil']
+			);
+		});
+	});
+});
+
+describe('Lexer (tokens)', function () {
+	describe(`'nil'`, function () {
+		it(`should produce the token`, function () {
+			const actual: TOKEN[] = lexer('nil');
+			expect(actual).to.eql(
+				['nil']
+			);
+		});
+	});
+	describe(`'mytoken'`, function () {
+		it(`should produce the token`, function () {
+			const actual: TOKEN[] = lexer('mytoken');
+			expect(actual).to.eql(
+				['mytoken']
+			);
+		});
+	});
+	describe(`'nilish'`, function () {
+		it(`should not split into two tokens`, function () {
+			const actual: TOKEN[] = lexer('nilish');
+			expect(actual).to.eql(
+				['nilish']
+			);
+		});
+	});
+	describe(`'my tokens'`, function () {
+		it(`should produce two separate tokens`, function () {
+			const actual: TOKEN[] = lexer('my token');
+			expect(actual).to.eql(
+				['my', 'token']
+			);
+		});
+	});
+	describe(`'...'`, function () {
+		it(`should produce a single 'dots' token`, function () {
+			const actual: TOKEN[] = lexer('...');
+			expect(actual).to.eql(
+				['...']
+			);
+		});
+	});
+});
+
+describe('Lexer (trees)', function () {
+	describe(`'<nil.nil>'`, function () {
+		it('should produce a matching token list', function () {
+			const actual: TOKEN[] = lexer('<nil.nil>');
+			expect(actual).to.eql(
+				[TKN_TREE_OPN, 'nil', TKN_DOT, 'nil', TKN_TREE_CLS]
+			);
+		});
+	});
+	describe(`'<<nil.nil>.nil>'`, function () {
+		it('should produce a matching token list', function () {
+			const actual: TOKEN[] = lexer('<<nil.nil>.nil>');
+			expect(actual).to.eql(
+				[TKN_TREE_OPN, TKN_TREE_OPN, 'nil', TKN_DOT, 'nil', TKN_TREE_CLS, TKN_DOT, 'nil', TKN_TREE_CLS]
+			);
+		});
+	});
+	describe(`'<nil.<nil.nil>>'`, function () {
+		it('should produce a matching token list', function () {
+			const actual: TOKEN[] = lexer('<nil.<nil.nil>>');
+			expect(actual).to.eql(
+				[TKN_TREE_OPN, 'nil', TKN_DOT, TKN_TREE_OPN, 'nil', TKN_DOT, 'nil', TKN_TREE_CLS, TKN_TREE_CLS]
+			);
+		});
+	});
+});
+
+describe('Lexer (lists)', function () {
+	describe(`'int[]'`, function () {
+		it('should produce a matching token list', function () {
+			const actual: TOKEN[] = lexer('int[]');
+			expect(actual).to.eql(
+				['int', TKN_LIST_OPN, TKN_LIST_CLS]
+			);
+		});
+	});
+	describe(`'(int|any)[]'`, function () {
+		it('should produce a matching token list', function () {
+			const actual: TOKEN[] = lexer('(int|any)[]');
+			expect(actual).to.eql(
+				[TKN_PREN_OPN, 'int', TKN_BAR, 'any', TKN_PREN_CLS, TKN_LIST_OPN, TKN_LIST_CLS]
+			);
+		});
+	});
+	describe(`'[int]'`, function () {
+		it('should produce a matching token list', function () {
+			const actual: TOKEN[] = lexer('[int]');
+			expect(actual).to.eql(
+				[TKN_LIST_OPN, 'int', TKN_LIST_CLS]
+			);
+		});
+	});
+	describe(`'[int, int]'`, function () {
+		it('should produce a matching token list', function () {
+			const actual: TOKEN[] = lexer('[int, int]');
+			expect(actual).to.eql(
+				[TKN_LIST_OPN, 'int', TKN_COMMA, 'int', TKN_LIST_CLS]
+			);
+		});
+	});
+	describe(`'[int,any]'`, function () {
+		it('should produce a matching token list', function () {
+			const actual: TOKEN[] = lexer('[int,any]');
+			expect(actual).to.eql(
+				[TKN_LIST_OPN, 'int', TKN_COMMA, 'any', TKN_LIST_CLS]
+			);
+		});
+	});
+	describe(`'[int, any]'`, function () {
+		it('should produce a matching token list', function () {
+			const actual: TOKEN[] = lexer('[int, any]');
+			expect(actual).to.eql(
+				[TKN_LIST_OPN, 'int', TKN_COMMA, 'any', TKN_LIST_CLS]
+			);
+		});
+	});
+
+	describe(`'[any, any[]]'`, function () {
+		it('should produce a matching token list', function () {
+			const actual: TOKEN[] = lexer('[any, any[]]');
+			expect(actual).to.eql(
+				[TKN_LIST_OPN, 'any', TKN_COMMA, 'any', TKN_LIST_OPN, TKN_LIST_CLS, TKN_LIST_CLS]
+			);
+		});
+	});
+	describe(`'[any, ...]'`, function () {
+		it('should produce a matching token list', function () {
+			const actual: TOKEN[] = lexer('[any, ...]');
+			expect(actual).to.eql(
+				[TKN_LIST_OPN, 'any', TKN_COMMA, TKN_DOTS, TKN_LIST_CLS]
+			);
+		});
+	});
+	describe(`'[..., any]'`, function () {
+		it('should produce a matching token list', function () {
+			const actual: TOKEN[] = lexer('[..., any]');
+			expect(actual).to.eql(
+				[TKN_LIST_OPN, TKN_DOTS, TKN_COMMA, 'any', TKN_LIST_CLS]
+			);
+		});
+	});
+	describe(`'[...]'`, function () {
+		it('should produce a matching token list', function () {
+			const actual: TOKEN[] = lexer('[...]');
+			expect(actual).to.eql(
+				[TKN_LIST_OPN, TKN_DOTS, TKN_LIST_CLS]
+			);
+		});
+	});
+});
+
+describe('Lexer (lexer errors)', function () {
+	function _errorMessage(char: string, position: number): string {
+		return `Unexpected token '${char}' at position ${position}`;
+	}
+
+	describe(`'-'`, function () {
+		it('Should throw an unexpected token error', function () {
+			expect(() => lexer('-')).to.throw(
+				_errorMessage('-', 0)
+			);
+		});
+	});
+
+	describe(`'nil-'`, function () {
+		it(`Should throw an unexpected token error at '-'`, function () {
+			expect(() => lexer('nil-')).to.throw(
+				_errorMessage('-', 3)
+			);
+		});
+	});
+});
