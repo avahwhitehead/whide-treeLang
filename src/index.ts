209c349d3889f473ff5fdec9fe1b689c81f278c1
Alaric Whitehead <alaric.whitehead@gmail.com>
2021-04-03 16:20:12 +0100

    feat: added exported methods to use this as a module

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import runConvert from "./converter";
+import { BinaryTree, ConvertedBinaryTree } from "./types/Trees";
+import lexer, { TOKEN } from "./lexer";
+import parser, { ConversionTree } from "./parser";
+
+/**
+ * Represents the result of a tree conversion.
+ */
+type ConversionResultType = {
+	/**
+	 * The result of the conversion
+	 */
+	tree: ConvertedBinaryTree,
+	/**
+	 * This is true if there is an error anywhere in the tree.
+	 * False if the tree was converted without any issues.
+	 */
+	error: boolean
+};
+
+/**
+ * Convert a binary tree using the provided conversion string
+ * @param tree				The tree to convert
+ * @param conversionString	The conversion string to use
+ * @returns		Object containing the result of the conversion
+ */
+export default function(tree: BinaryTree, conversionString: string): ConversionResultType {
+	//First lex the input into tokens
+	let tokens: TOKEN[] = lexer(conversionString);
+	//Then parse the result into a ConversionTree
+	let parseResult: ConversionTree = parser(tokens);
+	//Finally perform the conversion
+	return runConvert(tree, parseResult);
+}
