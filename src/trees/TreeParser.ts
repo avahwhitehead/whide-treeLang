f17b4e28e94a715b95ecef329b63fe97ffbbdf20
Alaric Whitehead <alaric.whitehead@gmail.com>
2021-05-02 18:16:06 +0100

    changed tree parser from using multiple ifs to a switch. removed type aliases from tree parser imports

--- src/trees/TreeParser.ts
+++ src/trees/TreeParser.ts
@@ -1,4 +1,4 @@
-import { TKN_TREE_OPN as OPEN, TKN_TREE_CLS as CLOSE, TKN_DOT as DOT } from "../../src/converter/lexer";
+import { TKN_TREE_OPN, TKN_TREE_CLS, TKN_DOT } from "../converter/lexer";
 import { TOKEN } from "./TreeLexer";
 import { _expect, _unexpectedToken } from "../utils/parser";
 import ParserException from "../exceptions/ParserException";
@@ -21,6 +21,26 @@ function _numberToTree(num: number): BinaryTree {
 }
 
 /**
+ * Produce a tree from a literal tree declaration ("<tree.tree>")
+ * @param tokens	The token list
+ */
+function _interpretTree(tokens: TOKEN[]): BinaryTree {
+	//Parse the left tree
+	const left : BinaryTree = _tokensToTree(tokens);
+	//Check for the dot separating the subtrees
+	_expect(tokens, TKN_DOT);
+	//Parse the right tree
+	const right : BinaryTree = _tokensToTree(tokens);
+	//Check the opening symbol has a matching closing symbol
+	_expect(tokens, TKN_TREE_CLS);
+	//Return the created tree
+	return {
+		left: left,
+		right: right,
+	};
+}
+
+/**
  * Parse a token list into a tree node
  * @param tokenList	The list of tokens
  */
@@ -31,28 +51,17 @@ function _tokensToTree(tokenList: TOKEN[]) : BinaryTree {
 	//Convert numbers directly to numbers
 	if (typeof token === "number") return _numberToTree(token);
 
-	//Accept `nil`
-	if (token === 'nil') return null;
-
-	//Accept binary trees
-	//"OPEN [TREE] DOT [TREE] CLOSE" ("<nil.nil>")
-	if (token === OPEN) {
-		//Parse the left tree
-		const left : BinaryTree = _tokensToTree(tokenList);
-		//Check for the dot separating the subtrees
-		_expect(tokenList, DOT);
-		//Parse the right tree
-		const right : BinaryTree = _tokensToTree(tokenList);
-		//Check the opening symbol has a matching closing symbol
-		_expect(tokenList, CLOSE);
-		//Return the created tree
-		return {
-			left: left,
-			right: right,
-		};
+	switch (token) {
+		//Accept `nil`
+		case 'nil':
+			return null;
+		//Accept binary trees
+		case TKN_TREE_OPN:
+			return _interpretTree(tokenList);
+		//Error otherwise
+		default:
+			throw _unexpectedToken(token);
 	}
-
-	throw _unexpectedToken(token);
 }
 
 /**
